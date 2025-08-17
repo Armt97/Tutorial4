d38d25a51daba1f76c0e4d5b4d29ca33bb58b6c6
Armt97 <144014054+Armt97@users.noreply.github.com>
2025-08-17 21:13:04 +1200

    Add failing subtraction test (CI should fail)

---
+++ src/test/java/org/example/Calc.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calc {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // temporary wrong implementation so the test fails
+    public int subtract(int a, int b) {
+        return 0;
+    }
+}
+
